8d3bec6b8a846668ec7117fb7697bc72b41968e7
Vitalii Vasilev <vasilev.v.vitalii@gmail.com>
2020-05-23 14:03:46 +0300

    refactor export types

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // @ts-check
-const app = require('./app.js')
+const lib_app = require('./app.js')
 const type = require('./@type.js')
 const get = require('./get.js')
 
 exports.create = create
 exports.get = get.get
 
+/**
+ * @typedef {lib_app} app
+ */
 /**
  * @typedef {type.constructor_options} http_constructor_options
  */
@@ -14,5 +17,5 @@ exports.get = get.get
  * @param {http_constructor_options} [options]
  */
 function create(options) {
-    return new app(options)
+    return new lib_app(options)
 }
